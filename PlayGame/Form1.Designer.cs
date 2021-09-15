55882384b4574dd1edab99e28019172544e762f3
P4 <P4@DESKTOP-0BG23MU>
2021-09-15 14:23:19 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.welcomeLabel1 = new System.Windows.Forms.Label();
+            this.welcomeLabel2 = new System.Windows.Forms.Label();
+            this.welcomeLabel3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Eras Medium ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(91, 194);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(198, 98);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "DO NOT PRESS";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // welcomeLabel1
+            // 
+            this.welcomeLabel1.AutoSize = true;
+            this.welcomeLabel1.Font = new System.Drawing.Font("Eras Medium ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel1.Location = new System.Drawing.Point(106, 22);
+            this.welcomeLabel1.Name = "welcomeLabel1";
+            this.welcomeLabel1.Size = new System.Drawing.Size(184, 43);
+            this.welcomeLabel1.TabIndex = 1;
+            this.welcomeLabel1.Text = "Welcome!";
+            // 
+            // welcomeLabel2
+            // 
+            this.welcomeLabel2.AutoSize = true;
+            this.welcomeLabel2.Font = new System.Drawing.Font("Eras Medium ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel2.Location = new System.Drawing.Point(75, 78);
+            this.welcomeLabel2.Name = "welcomeLabel2";
+            this.welcomeLabel2.Size = new System.Drawing.Size(237, 43);
+            this.welcomeLabel2.TabIndex = 2;
+            this.welcomeLabel2.Text = "DON\'T PRESS";
+            // 
+            // welcomeLabel3
+            // 
+            this.welcomeLabel3.AutoSize = true;
+            this.welcomeLabel3.Font = new System.Drawing.Font("Eras Medium ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel3.Location = new System.Drawing.Point(72, 134);
+            this.welcomeLabel3.Name = "welcomeLabel3";
+            this.welcomeLabel3.Size = new System.Drawing.Size(240, 43);
+            this.welcomeLabel3.TabIndex = 3;
+            this.welcomeLabel3.Text = "THE BUTTON";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PlayGame.Properties.Resources.scary;
+            this.pictureBox1.Location = new System.Drawing.Point(-67, -49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(520, 417);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(390, 304);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.welcomeLabel3);
+            this.Controls.Add(this.welcomeLabel2);
+            this.Controls.Add(this.welcomeLabel1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label welcomeLabel1;
+        private System.Windows.Forms.Label welcomeLabel2;
+        private System.Windows.Forms.Label welcomeLabel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer clickPlayer = new SoundPlayer(Properties.Resources.click);
+            SoundPlayer jumpscarePlayer = new SoundPlayer(Properties.Resources.jumpscare);
+            clickPlayer.Play();
+
+            Refresh();
+            Thread.Sleep(2000);
+
+            startButton.Hide();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel1.Text = "you...";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel2.Text = "shouldn't have...";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel3.Text = "done that! >:(";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel1.Hide();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel2.Hide();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            welcomeLabel3.Hide();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            jumpscarePlayer.Play();
+            pictureBox1.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+        }
+    }
+}
